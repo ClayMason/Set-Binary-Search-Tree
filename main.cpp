2d9dd36ef0d9f43890daeb3f55c8a84c77362c57
Muiz Yusuff <muiz.project@gmail.com>
2018-07-07 17:31:37 -0400

    Added test for iterator++ operation overload in main function

--- main.cpp
+++ main.cpp
@@ -12,5 +12,13 @@ int main () {
 
   std::cout << "A List: "  << a << std::endl;
 
+  // iterator test
+  std::cout << "Iterator test" << std::endl;
+  typename ds_set<int>::iterator itr;
+  for ( itr = a.begin(); itr != a.end(); ++itr ) {
+      std::cout << *itr << " ";
+  }
+  std::cout << std::endl;
+
   return 0;
 }
