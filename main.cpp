52a06be927b57ab1b328453be2330033357d0073
Muiz Yusuff <muiz.project@gmail.com>
2018-07-07 15:08:05 -0400

    Added insert tests for ds_set in main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
+#include "ds_set.h"
 
 int main () {
+
+  // DS_SET TESTS
+  ds_set<int> a;
+  // insertion test
+  for ( int i = 1; i < 20; i++ ) a.insert (i*3);
+  assert (a.size() == 19);
+
+  std::cout << "A List: "  << a << std::endl;
 
   return 0;
 }
